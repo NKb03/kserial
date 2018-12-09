34abf61023ce052633f9963fb6522fccd180b56f
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-09 18:54:11 +0100

    Add Binary IoFactory

--- src/main/kotlin/kserial/IOFactory.kt
+++ src/main/kotlin/kserial/IOFactory.kt
@@ -8,7 +8,13 @@ import java.io.InputStream
 import java.io.OutputStream
 
 interface IOFactory {
-    fun createInput(stream: InputStream)
+    fun createInput(stream: InputStream): Input
 
-    fun createOutput(stream: OutputStream)
+    fun createOutput(stream: OutputStream): Output
+
+    object Binary: IOFactory {
+        override fun createInput(stream: InputStream): BinaryInput = BinaryInput.fromStream(stream)
+
+        override fun createOutput(stream: OutputStream): Output = BinaryOutput.toStream(stream)
+    }
 }
