ca7b9ea0f80dfe5d703aaefe22a309da2395ffb7
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-09 18:58:09 +0100

    Add extensions to support path input and output

--- src/main/kotlin/kserial/IOFactory.kt
+++ src/main/kotlin/kserial/IOFactory.kt
@@ -5,13 +5,14 @@
 package kserial
 
 import java.io.*
+import java.nio.file.*
 
 interface IOFactory {
     fun createInput(stream: InputStream): Input
 
     fun createOutput(stream: OutputStream): Output
 
-    object Binary: IOFactory {
+    object Binary : IOFactory {
         override fun createInput(stream: InputStream): BinaryInput = BinaryInput.fromStream(stream)
 
         override fun createOutput(stream: OutputStream): Output = BinaryOutput.toStream(stream)
@@ -22,3 +23,6 @@ fun IOFactory.createInput(file: File) = createInput(FileInputStream(file))
 
 fun IOFactory.createOutput(file: File) = createOutput(FileOutputStream(file))
 
+fun IOFactory.createInput(path: Path) = createInput(Files.newInputStream(path))
+
+fun IOFactory.createOutput(path: Path) = createOutput(Files.newOutputStream(path))
