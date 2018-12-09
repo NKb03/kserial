853bc41e2c95d1c64461fce8fead58f5c1b259f4
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-09 19:13:30 +0100

    Add options parameter to utility extensions

--- src/main/kotlin/kserial/IOFactory.kt
+++ src/main/kotlin/kserial/IOFactory.kt
@@ -4,6 +4,8 @@
 
 package kserial
 
+import kserial.SerializationOption.ShareClassNames
+import kserial.SharingMode.Unshared
 import kserial.internal.BinaryInput
 import kserial.internal.BinaryOutput
 import java.io.*
@@ -14,23 +16,42 @@ import java.nio.file.Path
 interface IOFactory {
     fun createInput(stream: InputStream): Input
 
-    fun createOutput(stream: OutputStream): Output
+    fun createOutput(stream: OutputStream, vararg options: SerializationOption): Output
 
     object Binary : IOFactory {
         override fun createInput(stream: InputStream): BinaryInput = BinaryInput.fromStream(stream)
 
-        override fun createOutput(stream: OutputStream): Output = BinaryOutput.toStream(stream)
+        override fun createOutput(stream: OutputStream, vararg options: SerializationOption): Output {
+            val sharingMode = getSharingMode(options)
+            val shareClsNames = isShareClassNames(options)
+            return BinaryOutput.toStream(stream, sharingMode, shareClsNames)
+        }
+    }
+
+    companion object {
+        private fun getSharingMode(options: Array<out SerializationOption>): SharingMode {
+            val modes = options.filterIsInstance<SerializationOption.Sharing>()
+            require(modes.size <= 1) { "multiple sharing modes supplied" }
+            return modes.firstOrNull()?.mode ?: Unshared
+        }
+
+        private fun isShareClassNames(options: Array<out SerializationOption>): Boolean = ShareClassNames in options
     }
 }
 
-fun IOFactory.createInput(file: File) = createInput(FileInputStream(file))
+fun IOFactory.createInput(file: File) =
+    createInput(FileInputStream(file))
 
-fun IOFactory.createOutput(file: File) = createOutput(FileOutputStream(file))
+fun IOFactory.createOutput(file: File, vararg options: SerializationOption) =
+    createOutput(FileOutputStream(file), *options)
 
-fun IOFactory.createInput(path: Path) = createInput(Files.newInputStream(path))
+fun IOFactory.createInput(path: Path) =
+    createInput(Files.newInputStream(path))
 
-fun IOFactory.createOutput(path: Path) = createOutput(Files.newOutputStream(path))
+fun IOFactory.createOutput(path: Path, vararg options: SerializationOption) =
+    createOutput(Files.newOutputStream(path), *options)
 
 fun IOFactory.createInput(url: URL) = createInput(url.openStream())
 
-fun IOFactory.createOutput(url: URL) = createInput(url.openConnection().getInputStream())
+fun IOFactory.createOutput(url: URL, vararg options: SerializationOption) =
+    createOutput(url.openConnection().getOutputStream(), *options)
