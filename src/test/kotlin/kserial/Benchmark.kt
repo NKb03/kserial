5f3768ba4648967746d3bb4e290fa7ca79f6627c
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-10 17:57:03 +0100

    Make Benchmark output to console instead of spec

--- src/test/kotlin/kserial/Benchmark.kt
+++ src/test/kotlin/kserial/Benchmark.kt
@@ -4,16 +4,14 @@ import com.natpryce.hamkrest.Described
 import com.natpryce.hamkrest.should.describedAs
 import kserial.IOFactory.Binary
 import kserial.SerializationOption.Sharing
-import kserial.SharingMode.*
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.SpecBody
-import org.jetbrains.spek.api.dsl.describe
+import kserial.SharingMode.ShareEquivalent
 import java.io.*
 import kotlin.system.measureNanoTime
 
-internal object Benchmark : Spek({
-    describe("java serialization vs kserial") {
-        val tests = sequenceOf<Described<Any>>(
+internal object Benchmark {
+    @JvmStatic fun main(vararg args: String) {
+        waitForProfiler()
+        val testData = sequenceOf<Described<Any>>(
             Tree.construct(3) describedAs "a little tree",
             Tree.construct(5) describedAs "a larger tree",
             Tree.construct(7) describedAs "a really large tree",
@@ -22,90 +20,87 @@ internal object Benchmark : Spek({
             List(10000) { it } describedAs "a really large list of ints",
             List(10) { List(10) { idx -> idx } } describedAs "a nested list of ints",
             List(100) { List(100) { idx -> idx } } describedAs "a larger nested list of ints",
-            List(1000) { List(1000) { idx -> idx } } describedAs "a really large nested list of inst"
+            List(1000) { List(1000) { idx -> idx } } describedAs "a really large nested list of ints"
         )
         val context = SerialContext().apply {
             useUnsafe = true
         }
         val factory = IOFactory.Binary
-        for (t in tests) {
+        for (t in testData) {
             val desc = t.description
-            describe("serializing $desc") {
-                describe("with java") {
-                    serializeJava(t)
-                }
-                describe("with kserial") {
-                    serializeKSerial(factory, t, context, Unshared)
-                }
-                describe("with kserial sharing same") {
-                    serializeKSerial(factory, t, context, ShareSame)
-                }
-                describe("with kserial sharing equal") {
-                    serializeKSerial(factory, t, context, ShareEquivalent)
-                }
+            println("serializing $desc\n")
+            serializeJava(t)
+            serializeKSerial(factory, t, context, ShareEquivalent)
+            println("---------------------------------------")
+        }
+    }
+
+    private fun waitForProfiler() {
+        System.`in`.read()
+    }
+
+    private fun serializeKSerial(
+        factory: Binary,
+        t: Described<Any>,
+        context: SerialContext,
+        sharingMode: SharingMode
+    ) {
+        var sumWriteTime = 0.0
+        var sumReadTime = 0.0
+        var sumSize = 0.0
+        repeat(REPEAT_COUNT) {
+            val stream = ByteArrayOutputStream()
+            val out = factory.createOutput(stream, Sharing(sharingMode))
+            sumWriteTime += measureNanoTime { out.writeObject(t.value, context) }
+            val arr = stream.toByteArray()
+            sumSize += arr.size
+            val input = factory.createInput(arr)
+            sumReadTime += measureNanoTime { input.readObject(context) }
+        }
+        val averageWriteTime = sumWriteTime / REPEAT_COUNT / 1000.0
+        println("kserial serialization needs $averageWriteTime micros")
+        val averageReadTime = sumReadTime / REPEAT_COUNT / 1000.0
+        println("kserial deserialization needs $averageReadTime micros")
+        val averageSize = sumSize / REPEAT_COUNT
+        println("kserial deserialization needs $averageSize bytes")
+        println()
+    }
+
+    private fun serializeJava(t: Described<Any>) {
+        var sumWriteTime = 0.0
+        var sumReadTime = 0.0
+        var sumSize = 0.0
+        repeat(REPEAT_COUNT) {
+            val os = ByteArrayOutputStream()
+            val oos = ObjectOutputStream(os)
+            sumWriteTime += measureNanoTime { oos.writeObject(t.value) } / 1000.0
+            val arr = os.toByteArray()
+            sumSize += arr.size
+            val ois = ObjectInputStream(ByteArrayInputStream(arr))
+            sumReadTime += measureNanoTime { ois.readObject() } / 1000.0
+        }
+        val averageWriteTime = sumWriteTime / REPEAT_COUNT
+        println("java serialization needs $averageWriteTime micros")
+        val averageReadTime = sumReadTime / REPEAT_COUNT
+        println("java deserialization needs $averageReadTime micros")
+        val averageSize = sumSize / REPEAT_COUNT
+        println("java deserialization needs $averageSize bytes")
+        println()
+    }
+
+    private const val REPEAT_COUNT = 10
+
+
+    private data class Tree(val children: List<Tree>) : java.io.Serializable {
+        companion object {
+            val leaf = Tree(emptyList())
+
+            fun construct(deep: Int): Tree {
+                return if (deep == 0) leaf
+                else Tree(List(deep) { construct(deep - 1) })
             }
         }
     }
-})
 
-private fun SpecBody.serializeKSerial(
-    factory: Binary,
-    t: Described<Any>,
-    context: SerialContext,
-    sharingMode: SharingMode
-) {
-    var sumWriteTime = 0.0
-    var sumReadTime = 0.0
-    var sumSize = 0.0
-    repeat(REPEAT_COUNT) {
-        val stream = ByteArrayOutputStream()
-        val out = factory.createOutput(stream, Sharing(sharingMode))
-        sumWriteTime += measureNanoTime { out.writeObject(t.value, context) }
-        val arr = stream.toByteArray()
-        sumSize += arr.size
-        val input = factory.createInput(arr)
-        sumReadTime += measureNanoTime { input.readObject(context) }
-    }
-    val averageWriteTime = sumWriteTime / REPEAT_COUNT / 1000.0
-    test("kserial serialization needs $averageWriteTime micros") {}
-    val averageReadTime = sumReadTime / REPEAT_COUNT / 1000.0
-    test("kserial deserialization needs $averageReadTime micros") {}
-    val averageSize = sumSize / REPEAT_COUNT
-    test("kserial deserialization needs $averageSize bytes") {}
+
 }
-
-private fun SpecBody.serializeJava(t: Described<Any>) {
-    var sumWriteTime = 0.0
-    var sumReadTime = 0.0
-    var sumSize = 0.0
-    repeat(REPEAT_COUNT) {
-        val os = ByteArrayOutputStream()
-        val oos = ObjectOutputStream(os)
-        sumWriteTime += measureNanoTime { oos.writeObject(t.value) } / 1000.0
-        val arr = os.toByteArray()
-        sumSize += arr.size
-        val ois = ObjectInputStream(ByteArrayInputStream(arr))
-        sumReadTime += measureNanoTime { ois.readObject() } / 1000.0
-    }
-    val averageWriteTime = sumWriteTime / REPEAT_COUNT / 1000.0
-    test("java serialization needs $averageWriteTime micros") {}
-    val averageReadTime = sumReadTime / REPEAT_COUNT / 1000.0
-    test("java deserialization needs $averageReadTime micros") {}
-    val averageSize = sumSize / REPEAT_COUNT
-    test("java deserialization needs $averageSize bytes") {}
-}
-
-private const val REPEAT_COUNT = 10
-
-
-private data class Tree(val children: List<Tree>) : java.io.Serializable {
-    companion object {
-        val leaf = Tree(emptyList())
-
-        fun construct(deep: Int): Tree {
-            return if (deep == 0) leaf
-            else Tree(List(deep) { construct(deep - 1) })
-        }
-    }
-}
-
