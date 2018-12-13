09dd3c42ac0dfadcdeebc288a329b77d263e6906
Nikolaus Knop <niko.knop003@gmail.com>
2018-12-13 16:32:28 +0100

    Document InplaceSerializer

--- src/main/kotlin/kserial/InplaceSerializer.kt
+++ src/main/kotlin/kserial/InplaceSerializer.kt
@@ -4,8 +4,19 @@
 
 package kserial
 
+/**
+ * A Serializer that deserializes objects of type [T] by using the nullary constructor and then applying [deserialize]
+ */
 interface InplaceSerializer<in T : Any> {
+    /**
+     * Serialize the given [obj] writing to the [output]
+     * @param context the [SerialContext] used to instantiate serializers for nested objects
+     */
     fun serialize(obj: T, output: Output, context: SerialContext)
 
+    /**
+     * Deserialize the given [obj] reading from [input]
+     * @param context the [SerialContext] used to instantiate serializers for nested objects
+     */
     fun deserialize(obj: T, input: Input, context: SerialContext)
 }
